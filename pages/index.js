7eff9c2334ac5e730404ade38387455680258f35
Travelwiseiceland <travelwiseiceland@gmail.com>
2025-05-19 02:36:13 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Velkomin í TravelWise ✈️</h1>
+      <p>Smelltu hér til að leita að flugum:</p>
+      <a href="/flights" style={{ color: '#0070f3', textDecoration: 'underline' }}>➡️ Fara í flugleitarvél</a>
+    </div>
+  );
+}
